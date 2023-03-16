18991651f307dc401d0f3049deab043a6e7ab907
Anatolii <omelchenko20@gmail.com>
2023-03-16 16:46:31 +0200

    Added ErrorController

---
+++ WebSite/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Error", "PageNotFound");
+        }
+
+        public IActionResult PageNotFound()
+        {
+            return View();
+        }
+    }
+}
